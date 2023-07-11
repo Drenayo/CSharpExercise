58f52788aada83807da2acb5e893e41ad90d58ff
梦小天幼 <92071969+Drenayo@users.noreply.github.com>
2023-07-11 09:59:26 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
